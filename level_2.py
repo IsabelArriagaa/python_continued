3a6ff558dc4d47e28b158cfcfdd4ddb9638221fd
IsabelArriaga <a01722621@itesm.mx>
2025-04-24 08:56:39 -0400

    Level 2 Complete

---
+++ level_2.py
@@ -0,0 +1,9 @@
+rock_in_bag = True
+backpack_inventory_aka_mix_of_stuff = ["rock", "1", "that last thing was a rock", "1.5", "just kidding it's a rock", "rock", rock_in_bag, "rock"]
+print(backpack_inventory_aka_mix_of_stuff)
+
+playlist_for_coding = ["Pink Moon by Nick Drake", "Place to be by Nick Drake", "Road by Nick Drake", "Which Will by Nick Drake", "Horn by Nick Drake"]
+print(playlist_for_coding[0])
+
+playlist_for_coding.append("Never Gonna Give You up by Rick Astley")
+print (playlist_for_coding)
