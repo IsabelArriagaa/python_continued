c72c3e7357d60a4335c764de87bb8fb0955fa56d
IsabelArriaga <a01722621@itesm.mx>
2025-04-24 08:41:56 -0400

    Level 1 Complete

---
+++ level-one.py
@@ -0,0 +1,4 @@
+print("\"What do you call a ish with no eyes\" I paused, puzzled, until he grinned and said, \"Fsh!\" \n " \
+"\"I can't believe you'd say that to me.\" I said, questioning my friend's true knowledge of fish. \"Jellyfish are right there.\"")
+
+print("Line 1 \n Line 2 \n  Line 3 \n Line 4 \nLine 5")
